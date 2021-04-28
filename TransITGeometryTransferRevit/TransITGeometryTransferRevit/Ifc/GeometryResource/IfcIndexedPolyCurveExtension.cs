28ecd98b29888a428eac7d0261486c7dbdecd013
CouchFerret <services@couchferret.com>
2021-04-28 15:17:11 +0200

    Documenting IfcIndexedPolyCurveExtension but needs split refactor

--- TransITGeometryTransferRevit/TransITGeometryTransferRevit/Ifc/GeometryResource/IfcIndexedPolyCurveExtension.cs
+++ TransITGeometryTransferRevit/TransITGeometryTransferRevit/Ifc/GeometryResource/IfcIndexedPolyCurveExtension.cs
@@ -1,37 +1,34 @@
-﻿using Autodesk.Revit.DB;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+using Autodesk.Revit.DB;
+
 using Xbim.Common;
-using Xbim.Ifc;
 using Xbim.Ifc4.GeometricModelResource;
 using Xbim.Ifc4.GeometryResource;
 using Xbim.Ifc4.MeasureResource;
 
+
 namespace TransITGeometryTransferRevit.Ifc.GeometryResource
 {
+    /// <summary>
+    /// Extension methods to convert various IfcIndexedPolyCurve entities to Revit ones.
+    /// </summary>
     public static class IfcIndexedPolyCurveExtension
     {
-
+        /// <summary>
+        /// Recreates an IfcIndexedPolyCurve as a Revit (NurbSpline) Curve.
+        /// </summary>
+        /// <param name="ifcIndexedPolyCurve">The given curve to convert</param>
+        /// <returns>Returns a new NurbSpline Curve based on the poly curve</returns>
         public static Curve ToCurve(this IfcIndexedPolyCurve ifcIndexedPolyCurve)
         {
-
-            // Create a path curve
             List<XYZ> controlPoints = new List<XYZ>();
             List<double> weights = new List<double>();
 
-
             IfcCartesianPointList3D pointList = ifcIndexedPolyCurve.Points as IfcCartesianPointList3D;
             var coordList = pointList.CoordList;
-
-            var segment = ifcIndexedPolyCurve.Segments[0];
-
-            ;
-
-
-
 
             foreach (var coord in coordList)
             {
@@ -41,29 +38,27 @@ namespace TransITGeometryTransferRevit.Ifc.GeometryResource
 
             Curve pathCurve = NurbSpline.CreateCurve(controlPoints, weights);
 
-
             return pathCurve;
-
         }
 
+        /// <summary>
+        /// Recreates an IfcIndexedPolyCurve as a Revit CurveLoop. If the given curve is not closed, the new CurveLoop
+        /// will be closed with a simple Line segment between the end point and start point. Duplicated points at the
+        /// end of the polycurve is removed. Invalid segments by the point removal is also removed.
+        /// </summary>
+        /// <param name="ifcIndexedPolyCurve">The given curve to convert</param>
+        /// <returns>Returns a Closed CurveLoop based on the poly curve</returns>
         public static CurveLoop ToCurveLoop(this IfcIndexedPolyCurve ifcIndexedPolyCurve)
         {
-
-            List<XYZ> points = new List<XYZ>();
-
             IfcCartesianPointList3D pointList = ifcIndexedPolyCurve.Points as IfcCartesianPointList3D;
             var coordList = pointList.CoordList;
-
-
 
             while ((coordList[coordList.Count - 1].ToXYZ() - coordList[0].ToXYZ()).IsZeroLength())
             {
                 coordList.RemoveAt(coordList.Count - 1);
             }
 
-
             CurveLoop profile = new CurveLoop();
-
 
             foreach (var segment in ifcIndexedPolyCurve.Segments)
             {
@@ -84,46 +79,31 @@ namespace TransITGeometryTransferRevit.Ifc.GeometryResource
 
             }
 
-            var asd = profile.IsOpen();
-            ;
-
-            //var segment = ifcIndexedPolyCurve.Segments[0];
-
-            //;
-
-            //segment.ToCurve(coordList);
-
-
-            //foreach (var coord in coordList)
-            //{
-            //    points.Add(new XYZ(coord[0], coord[1], coord[2]));
-            //}
-
-            //while((points[points.Count - 1] - points[0]).IsZeroLength())
-            //{
-            //    ;
-            //    points.RemoveAt(points.Count - 1);
-            //}
-
-            //// TODO: Clean up! It shouldn't be profile specific
-
-            //for (int i = 0; i < points.Count-1; i++)
-            //{
-            //    profile.Append(Line.CreateBound(points[i], points[i+1]));
-            //}
-
-
-            //profile.Append(Line.CreateBound(points[points.Count-1], points[0]));
-
             return profile;
         }
 
+        /// <summary>
+        /// Converts an Ifc coordinate to a Revit XYZ.
+        /// </summary>
+        /// <param name="coord">The 3 dimensional Ifc coordinate to convert</param>
+        /// <returns>Returns the coordinate as a Revit XYZ coordinate</returns>
         private static XYZ ToXYZ(this IItemSet<IfcLengthMeasure> coord)
         {
             // TODO: Length check
+            if (coord.Count != 3)
+            {
+                throw new ArgumentOutOfRangeException("The given coordinate does not contain exactly 3 components.");
+            }
             return new XYZ(coord[0], coord[1], coord[2]);
         }
 
+        /// <summary>
+        /// Recreates an IfcSegmentIndexSelect and IItemSet<IItemSet<IfcLengthMeasure>> pair (Ifc way of defining
+        /// segments) as a Revit Curve (either Line or Arc).
+        /// </summary>
+        /// <param name="segment">The given segment containing the indices</param>
+        /// <param name="coordList">All the coordinates in the containing curve, not just the segment's coordinates</param>
+        /// <returns>Returns the segment as a Revit Curve, either Line or Arc</returns>
         private static Curve ToCurve(this IfcSegmentIndexSelect segment, IItemSet<IItemSet<IfcLengthMeasure>> coordList)
         {
             if (segment is IfcLineIndex line)
